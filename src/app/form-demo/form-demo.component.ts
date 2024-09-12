7d0cf443df388670b8af58372eeec03dad9e73a6
Nic McPhee <mcphee@morris.umn.edu>
2024-09-12 19:40:52 -0400

    Create a simple `FormDemoComponent`
    This shows several of the key elements of Angular reactive forms.

---
+++ src/app/form-demo/form-demo.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-demo',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './form-demo.component.html',
+  styleUrl: './form-demo.component.css',
+})
+export class FormDemoComponent {
+  text_display_form = new FormControl("You can't edit this");
+  text_editable_form = new FormControl('You can edit this');
+
+  form_not_submitted = true;
+
+  complex_form = new FormGroup({
+    first_name: new FormControl(''),
+    last_name: new FormControl(''),
+    company: new FormControl(''),
+  });
+
+  update_text_display_form() {
+    this.text_display_form.setValue(this.text_display_form.value + '!');
+  }
+
+  submit_complex_form() {
+    this.form_not_submitted = false;
+  }
+}
